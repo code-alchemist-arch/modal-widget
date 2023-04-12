fcc024e2e32925612c731b2f23f7b9caae714427
Miguel <miguelalexanderwork@gmail.com>
2023-04-12 15:06:12 -0500

    add smartAppBanner in index.html

--- src/App.js
+++ src/App.js
@@ -5,22 +5,7 @@ import TopBanner from './components/TopBanner';
 import AppContext from './context/app';
 
 function App() {
-  const smartAppBanner = {
-    _comment_: 'position has `float`, `bottom`, `top`, `top-banner`',
-    position: 'top',
-    iconIos: 'https://svgshare.com/i/ryz.svg',
-    iconAndroid: 'https://svgshare.com/i/rzS.svg',
-    textHeading: '<strong>Download now!</strong>',
-    textDescription: 'Try it now, download today',
-    buttonText: 'Download',
-    buttonColor: '#000000',
-    buttonLinkIos: 'https://appstore.com/app',
-    buttonLinkAndroid: 'https://playstore.com/app',
-    couponTextHeading: 'Get a discount on your first purchase',
-    couponTextDescription: 'Use coupon <strong>10OFF</strong>',
-    couponBackgroundColor: '#000',
-    couponTextColor: '#FFF',
-  };
+  const smartAppBanner = window._DEFAULT_DATA ?? {};
 
   return (
     <AppContext.Provider value={smartAppBanner}>
