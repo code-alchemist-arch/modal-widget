405922f7150677ce7402416b408f08dd1ab2a35b
Miguel <miguelalexanderwork@gmail.com>
2023-04-12 18:00:05 -0500

    add dom elements by js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,36 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 window.React = React;
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const existingRootDOM = document.getElementById('root');
+if (existingRootDOM) document.body.removeChild(existingRootDOM);
+const existingFloatModal = document.getElementById('float_modal');
+if (existingFloatModal) document.body.removeChild(existingFloatModal);
+const existingBottomPanel = document.getElementById('bottom_panel');
+if (existingBottomPanel) document.body.removeChild(existingBottomPanel);
+const existingTopBar = document.getElementById('top_bar');
+if (existingTopBar) document.body.removeChild(existingTopBar);
+const existingTopBanner = document.getElementById('top_banner');
+if (existingTopBanner) document.body.removeChild(existingTopBanner);
+
+const rootDOM = document.createElement('div');
+rootDOM.id = 'root';
+const floatModal = document.createElement('div');
+floatModal.id = 'float_modal';
+const bottomModal = document.createElement('div');
+bottomModal.id = 'bottom_panel';
+const topBar = document.createElement('div');
+topBar.id = 'top_bar';
+const topBanner = document.createElement('div');
+topBanner.id = 'top_banner';
+
+document.body.appendChild(rootDOM);
+document.body.appendChild(floatModal);
+document.body.appendChild(bottomModal);
+document.body.appendChild(topBar);
+document.body.appendChild(topBanner);
+
+const root = ReactDOM.createRoot(rootDOM);
 root.render(
   <React.StrictMode>
     <App />
